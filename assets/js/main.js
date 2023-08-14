aa52bdeb6d9576f926f846317087ba9b7acc0d3f
nikthewik <nikthewik@gmail.com>
2023-08-15 01:03:53 +0200

    Game now is working as intended: both + and - btns are useful

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,8 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// FUNCTIONS DEFINED
-
-// To Create Elements
+// ELEMENTS
+// Function To Create Elements
 function createEl(htmlTag, cssClass, text, parentEl) {
   // 1. Creating HTML Element
   let el = document.createElement(htmlTag);
@@ -18,139 +17,14 @@ function createEl(htmlTag, cssClass, text, parentEl) {
   return el;
 }
 
-// To Control The Font-size When There Is Text Instead Of Numbers On Display
-function setFontSize() {
-  display.classList.remove("num-font-size");
-  display.classList.add("quest-font-size");
-}
-
-// To Reset Font-size To Initial Value
-function resetFontSize() {
-  display.classList.remove("quest-font-size");
-  display.classList.add("num-font-size");
-}
-
-// To Set Color Interface To Red
-function setColorsToRed() {
-  if (counter < 0) {
-    colorNotch.setAttribute("content", "#ff0000");
-    title.classList.remove("normal-text");
-    title.classList.add("alarm-text");
-    container.classList.remove("normal-border");
-    container.classList.add("alarm-border");
-    display.classList.remove("normal-text");
-    display.classList.add("alarm-text");
-    plusBtn.classList.remove("normal-text", "normal-border");
-    plusBtn.classList.add("alarm-text", "alarm-border");
-    resetBtn.classList.remove("normal-text", "normal-border");
-    resetBtn.classList.add("alarm-text", "alarm-border");
-    minusBtn.classList.remove("normal-text", "normal-border");
-    minusBtn.classList.add("alarm-text", "alarm-border");
-    info.classList.add("alarm-icon");
-    volume.classList.add("alarm-icon");
-    github.classList.add("alarm-icon");
-    cap.classList.remove("normal-text");
-    cap.classList.add("alarm-text");
-    footer.classList.remove("normal-text");
-    footer.classList.add("alarm-text");
-    alarmFilter.classList.remove("none");
-    alarmFilter.classList.add("alarm-filter");
-  } else {
-    resetColors();
-  }
-}
-
-// To Reset Color Interface To Normal
-function resetColors() {
-  colorNotch.setAttribute("content", "#ececec");
-  title.classList.remove("alarm-text");
-  title.classList.add("normal-text");
-  container.classList.remove("alarm-border");
-  container.classList.add("normal-border");
-  display.classList.remove("alarm-text");
-  display.classList.add("normal-text");
-  plusBtn.classList.remove("alarm-text", "alarm-border");
-  plusBtn.classList.add("normal-text", "normal-border");
-  resetBtn.classList.remove("alarm-text", "alarm-border");
-  resetBtn.classList.add("normal-text", "normal-border");
-  minusBtn.classList.remove("alarm-text", "alarm-border");
-  minusBtn.classList.add("normal-text", "normal-border");
-  info.classList.remove("alarm-icon");
-  volume.classList.remove("alarm-icon");
-  github.classList.remove("alarm-icon");
-  cap.classList.remove("alarm-text");
-  cap.classList.add("normal-text");
-  footer.classList.remove("alarm-text");
-  footer.classList.add("normal-text");
-  alarmFilter.classList.remove("alarm-filter");
-  alarmFilter.classList.add("none");
-}
-
-// To Play The Audio And The Background Video
-function playMedia() {
-  if (video.paused && audio.paused) {
-    video.play();
-    audio.play();
-  }
-}
-
-// To Speed Up The Background Video
-function setPlayback() {
-  if (counter < 10) {
-    video.playbackRate = 1;
-  } else if (counter >= 10 && counter < 20) {
-    video.playbackRate = 1.5;
-  } else if (counter >= 20 && counter < 30) {
-    video.playbackRate = 2;
-  } else if (counter >= 30 && counter < 40) {
-    video.playbackRate = 2.5;
-  } else if (counter >= 40 && counter < 50) {
-    video.playbackRate = 3;
-  } else if (counter >= 50 && counter < 60) {
-    video.playbackRate = 3.5;
-  } else if (counter >= 60 && counter < 70) {
-    video.playbackRate = 4;
-  } else if (counter >= 70 && counter < 80) {
-    video.playbackRate = 4.5;
-  } else if (counter >= 80 && counter < 90) {
-    video.playbackRate = 5.2;
-  } else if (counter >= 90 && counter <= 100) {
-    video.playbackRate = 6;
-  }
-}
-
-// To Mute The Audio
-function muteAudio() {
-  audio.volume = 0;
-  volume.setAttribute("src", "/assets/img/volume-off.png");
-}
-
-// To Unmute The Audio
-function unmuteAudio() {
-  audio.volume = 1;
-  volume.setAttribute("src", "/assets/img/volume-on.png");
-}
-
-// To Reset And Stop The Audio And The Background Video
-function resetMedia() {
-  video.currentTime = 0;
-  audio.currentTime = 0;
-  video.pause();
-  audio.pause();
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-// ELEMENTS
-
-// Elements obtained
+// ELEMENTS OBTAINED
 // Meta Tag For The Color Of The Notch On iPhone
 let colorNotch = document.querySelector('meta[name="theme-color"]');
 
 // - Body
 const body = document.querySelector("body");
 
-// Elements created
+// ELEMENTS CREATED
 // -- Page
 const page = createEl("div", "page flex flex-cc-col", "", body);
 
@@ -289,14 +163,19 @@ if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
 
 // VARIABLES
 
-let counter = 0;
-let gameOver = false;
+let counter;
+let gameOver;
+let isPlusBtnPressed = false;
+let isMinusBtnPressed = false;
+let isPlusBtnBlocked = false;
+let isMinusBtnBlocked = false;
 let possibility;
 let questReward;
+let questCounter;
 
 const positiveQuests = [
   `You find an ancient treasure on the planet Makhaar. Move forward in your adventure by ${getQuestReward()} light years! 💎`,
-  `You eat the best icecream of the entire Cosmo on the planet Paradice. Move forward in your adventure by ${getQuestReward()} light years! 🍧`,
+  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by ${getQuestReward()} light years! 🍧`,
   `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by ${getQuestReward()} light years! 🏴‍☠️`,
   `You intercept a mysterious alien trasmission asking for help. Move forward in your adventure by ${getQuestReward()} light years! 👽`,
   `You meet Lyssa Grey Glove: "Hide over here! The Dark Army is coming!". Move forward in your adventure by ${getQuestReward()} light years!  🥷🏻`,
@@ -307,6 +186,192 @@ const negativeQuests = [
   `You encounter a massive gravitational anomaly that pulls your ship off course. Move backwards by ${getQuestReward()} light years! ⚫️`,
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by ${getQuestReward()} light years! ⛽️`,
 ];
+
+////////////////////////////////////////////////////////////////////////////////
+
+// FUNCTIONS
+
+function initGame() {
+  gameOver = false;
+  counter = 0;
+  display.innerHTML = counter;
+}
+
+initGame();
+
+function playGame() {
+  if (gameOver === false && counter > -99 && counter < 99) {
+    playMedia();
+
+    if (isPlusBtnPressed === true) {
+      counter++;
+    } else if (isMinusBtnPressed === true) {
+      counter--;
+    }
+    manageQuestPossibility();
+  } else {
+    setGameOver();
+  }
+  setPlayback();
+  setColorsToRed();
+}
+
+function setGameOver() {
+  setFontSize();
+  display.innerHTML = counter > 0 ? "You Win!" : "You Lost!";
+  gameOver = true;
+}
+
+function resetGame() {
+  resetMedia();
+  resetFontSize();
+  resetColors();
+  initGame();
+  questCounter = 0;
+  setQuestCounter();
+}
+
+// To Play The Audio And The Background Video
+function playMedia() {
+  if (video.paused && audio.paused) {
+    video.play();
+    audio.play();
+  }
+}
+
+// To Reset And Stop The Audio And The Background Video
+function resetMedia() {
+  video.currentTime = 0;
+  video.pause();
+  audio.currentTime = 0;
+  audio.pause();
+}
+
+// To Speed Up The Background Video
+function setPlayback() {
+  if (counter < 10) {
+    video.playbackRate = 1;
+  } else if (counter >= 10 && counter < 20) {
+    video.playbackRate = 1.5;
+  } else if (counter >= 20 && counter < 30) {
+    video.playbackRate = 2;
+  } else if (counter >= 30 && counter < 40) {
+    video.playbackRate = 2.5;
+  } else if (counter >= 40 && counter < 50) {
+    video.playbackRate = 3;
+  } else if (counter >= 50 && counter < 60) {
+    video.playbackRate = 3.5;
+  } else if (counter >= 60 && counter < 70) {
+    video.playbackRate = 4;
+  } else if (counter >= 70 && counter < 80) {
+    video.playbackRate = 4.5;
+  } else if (counter >= 80 && counter < 90) {
+    video.playbackRate = 5.2;
+  } else if (counter >= 90 && counter <= 100) {
+    video.playbackRate = 6;
+  }
+}
+
+// To Mute The Audio
+function muteAudio() {
+  audio.volume = 0;
+  volume.setAttribute("src", "/assets/img/volume-off.png");
+}
+
+// To Unmute The Audio
+function unmuteAudio() {
+  audio.volume = 1;
+  volume.setAttribute("src", "/assets/img/volume-on.png");
+}
+
+// To Control The Font-size When There Is Text Instead Of Numbers On Display
+function setFontSize() {
+  display.classList.remove("num-font-size");
+  display.classList.add("quest-font-size");
+}
+
+// To Reset Font-size To Initial Value
+function resetFontSize() {
+  display.classList.remove("quest-font-size");
+  display.classList.add("num-font-size");
+}
+
+// To Set Color Interface To Red
+function setColorsToRed() {
+  if (counter < 0) {
+    colorNotch.setAttribute("content", "#ff0000");
+    title.classList.remove("normal-text");
+    title.classList.add("alarm-text");
+    container.classList.remove("normal-border");
+    container.classList.add("alarm-border");
+    display.classList.remove("normal-text");
+    display.classList.add("alarm-text");
+    plusBtn.classList.remove("normal-text", "normal-border");
+    plusBtn.classList.add("alarm-text", "alarm-border");
+    resetBtn.classList.remove("normal-text", "normal-border");
+    resetBtn.classList.add("alarm-text", "alarm-border");
+    minusBtn.classList.remove("normal-text", "normal-border");
+    minusBtn.classList.add("alarm-text", "alarm-border");
+    info.classList.add("alarm-icon");
+    volume.classList.add("alarm-icon");
+    github.classList.add("alarm-icon");
+    cap.classList.remove("normal-text");
+    cap.classList.add("alarm-text");
+    footer.classList.remove("normal-text");
+    footer.classList.add("alarm-text");
+    alarmFilter.classList.remove("none");
+    alarmFilter.classList.add("alarm-filter");
+  } else {
+    resetColors();
+  }
+}
+
+// To Reset Color Interface To Normal
+function resetColors() {
+  colorNotch.setAttribute("content", "#ececec");
+  title.classList.remove("alarm-text");
+  title.classList.add("normal-text");
+  container.classList.remove("alarm-border");
+  container.classList.add("normal-border");
+  display.classList.remove("alarm-text");
+  display.classList.add("normal-text");
+  plusBtn.classList.remove("alarm-text", "alarm-border");
+  plusBtn.classList.add("normal-text", "normal-border");
+  resetBtn.classList.remove("alarm-text", "alarm-border");
+  resetBtn.classList.add("normal-text", "normal-border");
+  minusBtn.classList.remove("alarm-text", "alarm-border");
+  minusBtn.classList.add("normal-text", "normal-border");
+  info.classList.remove("alarm-icon");
+  volume.classList.remove("alarm-icon");
+  github.classList.remove("alarm-icon");
+  cap.classList.remove("alarm-text");
+  cap.classList.add("normal-text");
+  footer.classList.remove("alarm-text");
+  footer.classList.add("normal-text");
+  alarmFilter.classList.remove("alarm-filter");
+  alarmFilter.classList.add("none");
+}
+
+function manageQuestPossibility() {
+  possibility = Math.floor(Math.random() * 100 + 1);
+  console.log(possibility);
+  if (possibility > 0 && possibility <= 93) {
+    resetFontSize();
+    display.innerHTML = counter;
+  } else if (possibility >= 94 && possibility <= 98) {
+    setFontSize();
+    display.innerHTML = getRandomQuest(positiveQuests);
+    isMinusBtnBlocked = true;
+    isPlusBtnBlocked = false;
+    questCounter = questReward;
+  } else if (possibility >= 99 && possibility <= 100) {
+    setFontSize();
+    display.innerHTML = getRandomQuest(negativeQuests);
+    isPlusBtnBlocked = true;
+    isMinusBtnBlocked = false;
+    questCounter = questReward;
+  }
+}
 
 function getRandomQuest(questArray) {
   const randomIndex = Math.floor(Math.random() * questArray.length);
@@ -319,51 +384,25 @@ function getQuestReward() {
   return questReward;
 }
 
-function setGameOver() {
-  setFontSize();
-  display.innerHTML = counter > 0 ? "You Win!" : "You Lost!";
-  gameOver = true;
-}
-
-function manageQuestPossibility() {
-  possibility = Math.floor(Math.random() * 10 + 1);
-  if (possibility > 0 && possibility <= 8) {
-    resetFontSize();
-    display.innerHTML = counter;
-  } else if (possibility === 9) {
-    setFontSize();
-    display.innerHTML = getRandomQuest(positiveQuests);
-    counter += questReward;
-  } else if (possibility === 10) {
-    setFontSize();
-    display.innerHTML = getRandomQuest(negativeQuests);
-    counter -= questReward;
+function setQuestCounter() {
+  if (questCounter === 0) {
+    isPlusBtnBlocked = false;
+    isMinusBtnBlocked = false;
+  } else {
+    questCounter--;
   }
 }
 
-function resetGame() {
-  resetMedia();
-  resetFontSize();
-  resetColors();
-  gameOver = false;
-  counter = 0;
-  display.innerHTML = counter;
-}
 ////////////////////////////////////////////////////////////////////////////////
 
 // EVENTS
 
 plusBtn.addEventListener("click", () => {
-  if (gameOver === false) {
-    if (counter > -99 && counter < 99) {
-      playMedia();
-      counter++;
-      manageQuestPossibility();
-    } else {
-      setGameOver();
-    }
-    setPlayback();
-    setColorsToRed();
+  if (isPlusBtnBlocked === false) {
+    isPlusBtnPressed = true;
+    playGame();
+    setQuestCounter();
+    isPlusBtnPressed = false;
   }
 });
 
@@ -372,16 +411,11 @@ resetBtn.addEventListener("click", () => {
 });
 
 minusBtn.addEventListener("click", () => {
-  if (gameOver === false) {
-    if (counter > -99 && counter < 99) {
-      playMedia();
-      counter--;
-      manageQuestPossibility();
-    } else {
-      setGameOver();
-    }
-    setPlayback();
-    setColorsToRed();
+  if (isMinusBtnBlocked === false) {
+    isMinusBtnPressed = true;
+    playGame();
+    setQuestCounter();
+    isMinusBtnPressed = false;
   }
 });
 
