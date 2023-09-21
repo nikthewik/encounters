997a62eb1bd6fb1137043b5c90bd56e011b0336b
nikthewik <nikthewik@gmail.com>
2023-09-21 20:04:47 +0200

    Added all the positive quests.

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,38 +253,38 @@ let recordEncounters = 0;
 // Positive Quests
 const positiveQuests = [
   // 5 Points Quests
-  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by 5 light years! 🍧`,
-  `You find an ancient treasure on the planet Makhaar. Move forward in your adventure by 5 light years! 💍`,
-  `You intercept a mysterious alien trasmission asking for help. Move forward in your adventure by 5 light years! 👽`,
-  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by 5 light years! 🍧`,
-  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by 5 light years! 🍧`,
-  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by 5 light years! 🍧`,
-  `You eat the best icecream of the entire Cosmo on the planet ParadIce. Move forward in your adventure by 5 light years! 🍧`,
+  `You eat the best ice cream of the entire Universe on the planet ParadIce. Move FORWARD in your adventure by 5 light years! 🍧`,
+  `You uncover an ancient alien star map on the planet Makhaar. Move FORWARD in your adventure by 5 light years! 🗺️`,
+  `You land on a party moon and join an intergalactic feast. Move FORWARD by 5 light years, but first... Let's dance! 🕺🏻`,
+  `You stumble upon a Space Café where the owner insists you to try their famous cosmic cappuccino. Move FORWARD by 5 light years! 🥤`,
+  `You win the Race2Space competition. As a prize, you get a lot of solid propellant to move FORWARD by 5 light years! 🚀`,
+  `You rescue some space travelers in danger. As thanks, they share coordinates of a wormhole. Move FORWARD by 5 light years! 🆘`,
+  `NX-3000 joins you! He is a powerful AI companion, but also a nice friend. Your OS moves FORWARD by 5 light years, just as you! 🤖`,
   // 7 Points Quests
-  `You meet Lyssa Grey Glove: "Soon, hide over here! The Dark Army is coming!". Move forward in your adventure by 7 light years!  🥷🏻`,
-  `You meet Tarta, the Omniscient: "I'll tell you a secret... but slowly!". Move forward in your adventure by 7 light years! 🐢`,
-  `You meet De-Lo, Dead Lotus: "Ahahah you’re a strange guy! All right, I’ll help you." Move forward in your adventure by 7 light years! 🪷`,
-  `You find an ancient treasure on the planet Makhaar. Move forward in your adventure by 7 light years! 💎`,
-  `You find an ancient treasure on the planet Makhaar. Move forward in your adventure by 7 light years! 💎`,
-  `You find an ancient treasure on the planet Makhaar. Move forward in your adventure by 7 light years! 💎`,
+  `You meet Lyssa Grey Glove: "Soon, hide over here! The Dark Army is coming!". Move FORWARD in your adventure by 7 light years!  🥷🏻`,
+  `You meet Tarta, the Omniscient: "I'll tell you about my vision... but slowly!". Move FORWARD in your adventure by 7 light years! 🐢`,
+  `You meet De-Lo, Dead Lotus: "Do you need some brute force? Ahahah all right, I’ll join you.". Move FORWARD by 7 light years! 🪷`,
+  `Marcus, the M-astrophysicist joins you: “Everything as planned. I optimised the route.”. Move FORWARD by 7 light years! 🧑🏻‍🔬`,
+  `BeatriX D/Headshot joins you: “An anti-matter sniper rifle and a beer?! Wow, I'm totally in!”. Move FORWARD by 7 light years! 🍻`,
+  `You successfully negotiate a trade alliance with an alien nation, gaining advanced propulsion tech. Move FORWARD by 7 light years! 🫱🏻‍🫲🏾`,
   // 10 Points Quests
-  `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by 10 light years! 🏴‍☠️`,
-  `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by 10 light years! 🏴‍☠️`,
-  `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by 10 light years! 🏴‍☠️`,
-  `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by 10 light years! 🏴‍☠️`,
-  `You join the Space Pirate Brigade. You really feel free. Move forward in your adventure by 10 light years! 🏴‍☠️`,
+  `You establish the Space Pirates Brigade. You really feel free. Move FORWARD in your adventure by 10 light years! 🏴‍☠️`,
+  `The Galactic Council commends your achievements, granting you an interstellar boost. Move FORWARD by 10 light years! 🏛️`,
+  `You infiltrate the Dark Army’s data vault, extracting critical intel about its evil affairs. Move FORWARD by 10 light years! 💾`,
+  `You enter the Sakura Cosmo: a region of space full of cherry blossoms. You feel very grateful. Move FORWARD by 10 light years! 🌸`,
+  `A space bard serenades you with a cosmic ballad, inspiring you to venture FORWARD by 10 light years! 🎶`,
   // 30 Points Teleporting Quests
-  `You discover a peculiar anomaly that creates a quantum link with a distant star. Move INSTANTLY forward by 30 light years! 🌟`,
-  `You discover a peculiar anomaly that creates a quantum link with a distant star. Move INSTANTLY forward by 30 light years! 🌟`,
+  `You discover a peculiar anomaly that creates a quantum link with a distant star. Move INSTANTLY FORWARD by 30 light years! 🌟`,
+  `You discover a peculiar anomaly that creates a quantum link with a distant star. Move INSTANTLY FORWARD by 30 light years! 🌟`,
 ];
 
 // Negative Quests
 const negativeQuests = [
   // 5 Points Quests
-  `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
-  `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
-  `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
-  `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
+  `Oh no, an holographic trap! That spaceship wasn’t real. You’ve wasted so much fuel to follow it. Move BACK by 5 light years! ⛽️`,
+  `You contract a rare type of space virus, requiring immediate quarantine. Move BACK by 5 light years! 🦠`,
+  `You activate a space-time dilation field by mistake, moving BACK by 5 light years! ⏳`,
+  `Your energy reserves run critically low, requiring you to move BACK of 5 light years to recharge! 🪫`,
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 5 light years! ⛽️`,
@@ -302,8 +302,8 @@ const negativeQuests = [
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 10 light years! ⛽️`,
   `Oh no, an holographic trap! That ship was a misdirection, you’ve wasted so much fuel... Move backwards by 10 light years! ⛽️`,
   // 30 Points Teleporting Quests
-  `You encounter a super massive gravitational anomaly that pulls your ship off course. Move INSTANTLY backwards by 30 light years! ⚫️`,
-  `You encounter a super massive gravitational anomaly that pulls your ship off course. Move INSTANTLY backwards by 30 light years! ⚫️`,
+  `You encounter a super massive gravitational anomaly that pulls your ship off course. Move INSTANTLY BACK by 30 light years! ⚫️`,
+  `You encounter a super massive gravitational anomaly that pulls your ship off course. Move INSTANTLY BACK by 30 light years! ⚫️`,
 ];
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -367,16 +367,16 @@ function manageQuestPossibility() {
 
   console.log(`Possibility: ${possibility}`);
 
-  if (possibility > 0 && possibility <= 80) {
+  if (possibility > 0 && possibility <= 72) {
     resetFontSize();
     display.innerHTML = counter;
-  } else if (possibility >= 81 && possibility <= 90) {
+  } else if (possibility >= 73 && possibility <= 86) {
     setFontSize();
     display.innerHTML = getRandomQuest(positiveQuests);
     isMinusBtnBlocked = true;
     isPlusBtnBlocked = false;
     isPositiveQuest = true;
-  } else if (possibility >= 91 && possibility <= 100) {
+  } else if (possibility >= 87 && possibility <= 100) {
     setFontSize();
     display.innerHTML = getRandomQuest(negativeQuests);
     isPlusBtnBlocked = true;
@@ -386,7 +386,7 @@ function manageQuestPossibility() {
 
   setQuestCountdown();
 
-  if (possibility > 80) {
+  if (possibility > 72) {
     totalEncounters++;
     title.innerHTML = `enCounters = ${totalEncounters}`;
   }
@@ -411,7 +411,7 @@ function getQuestReward() {
     questReward = 10;
   } else if (randomIndex >= 18 && randomIndex <= 19) {
     questReward = 1;
-    if (possibility > 90) {
+    if (possibility > 86) {
       counter -= 30;
     } else {
       counter += 30;
@@ -465,7 +465,7 @@ function setGameOver() {
     }
   } else {
     display.innerHTML =
-      "The Dark Army captures you and sells you as a slave on the planet Kragas. You are destined to die in chains. YOU LOST! 💀";
+      "The Dark Army captures you and sells you as a slave on the planet Kragas. You are destined to die alone, in chains. YOU LOSE! 💀";
   }
   isGameOver = true;
 }
